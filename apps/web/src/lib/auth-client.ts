1a5278182628ee47af34683aa99eb1b909114388
KyroShaw <choola944@gmail.com>
2026-06-24 17:40:34 +0800

    feat: init

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@coin-pilot/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.VITE_SERVER_URL,
+});
